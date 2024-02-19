8081da07500b8c3efe8cbe58260512bea82a1e1d
Abigail Ramsøe <abigail@palaeome.org>
2024-02-19 09:49:36 +0100

    simplify mapping config

--- helpers/make_units-mapping.sh
+++ helpers/make_units-mapping.sh
@@ -1,13 +1,11 @@
 
 fqfol=$1
 
-echo -e "R1\tR2\tsm\tlb\tid\tidlane"
+echo -e "R1\tR2\tsm\tlb\tid"
 for r1 in $(ls $fqfol/*_R1_001.fastq.gz)
 do
   sm=$(basename $r1|cut -f1 -d-|cut -c1-6)
   lb=$(basename $r1|cut -f3 -d-)
-  id=$(basename $r1|cut -f1 -d_)
-  id_wlane=$(basename $r1|cut -f1-3 -d_)
   r2=$(echo $r1|sed 's|R1_001|R2_001|g')
-  echo -e "$(realpath $r1)\t$(realpath $r2)\t$sm\t$lb\t$id\t$id_wlane"
+  echo -e "$(realpath $r1)\t$(realpath $r2)\t$sm\t$lb"
 done
